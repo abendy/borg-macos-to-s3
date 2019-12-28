89a102f12166858c2debebe401997990641d0546
Allan Bendy <allan.bendy@gmail.com>
2019-12-28 13:36:10 -0500

    truncate log file on each run

--- borg-backup.sh
+++ borg-backup.sh
@@ -37,6 +37,9 @@ function main () {
   # Keep-alive: update existing `sudo` time stamp until the script has finished
   while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+  # Truncate log file
+  : > ${BORG_LOG_FILE}
+
   # Backup
   borg create                                                   \
     --compression zlib,6                                        \
