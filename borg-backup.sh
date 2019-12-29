a59fa0b17234cc00f68898f3496dcdf9c79601df
Allan Bendy <allan.bendy@gmail.com>
2019-12-28 23:48:58 -0500

    fixes permission error

--- borg-backup.sh
+++ borg-backup.sh
@@ -97,7 +97,7 @@ function main () {
 
 function alert () {
   FILENAME=$(basename "${BORG_LOG_FILE%}")
-  ATTACHMENT=`$BORG_LOG_FILE | /usr/bin/base64`
+  ATTACHMENT=`/usr/bin/base64 -i $BORG_LOG_FILE`
   BODY=`echo "$1" | /usr/bin/base64`
 
   TEMPLATE="ses-email-template.json"
